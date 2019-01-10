21dd3d22764c80e3f5889dd3c7f9e4440ac21f00
Siarhei Ladutska <siarhei_ladutska@epam.com>
2019-01-10 12:10:30 +0300

    feat: add WeatherListComponent

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -2,7 +2,9 @@ import angular from 'angular';
 
 import AppComponent from './app.component';
 import WeatherItemComponent from './weather-item/weather-item.component';
+import WeatherListComponent from './weather-list/weather-list.component';
 
 angular.module('weather-app', [])
     .component('app', AppComponent)
-    .component('weatherItem', WeatherItemComponent);
+    .component('weatherItem', WeatherItemComponent)
+    .component('weatherList', WeatherListComponent);
---
+++ src/app/weather-list/weather-list.component.js
@@ -0,0 +1,36 @@
+import template from './weather-list.component.html';
+import './weather-list.component.css';
+
+class controller {
+    constructor() {
+        this.weathers = [
+            {
+                id: '1',
+                location: 'Minsk',
+                img: 'assets/img/sunny.png',
+                temp: 25,
+            },
+            {
+                id: '2',
+                location: 'Paris',
+                img: 'assets/img/rain.png',
+                temp: 20,
+            },
+            {
+                id: '3',
+                location: 'London',
+                img: 'assets/img/clouds.png',
+                temp: 15,
+            },
+        ];
+    }
+
+    onCloseClick(id) {
+        console.log(id);
+    }
+}
+
+export default {
+    template,
+    controller,
+}
