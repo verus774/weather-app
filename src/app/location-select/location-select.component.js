5ff4cee842effba030c337c957dd0b3829f5afd9
Siarhei Ladutska <siarhei_ladutska@epam.com>
2019-01-11 12:16:43 +0300

    fix: remove selected location from dropdown and return it after weather card deleting

--- src/app/location-select/location-select.component.js
+++ src/app/location-select/location-select.component.js
@@ -2,18 +2,30 @@ import template from './location-select.component.html';
 import './location-select.component.css';
 
 class controller {
-    constructor(weatherService) {
+    constructor($rootScope, weatherService) {
+        this.$rootScope = $rootScope;
         this.weatherService = weatherService;
     }
 
     $onInit() {
         this.currLocation = '';
+        this.locations = [];
+
         this.weatherService.getLocations()
             .then(locations => this.locations = locations);
+
+        this.onWeatherCloseUnsub = this.$rootScope.$on('onWeatherClose', (event, data) => {
+            this.locations.push(data.location);
+        });
+    }
+
+    $onDestroy() {
+        this.onWeatherCloseUnsub();
     }
 
     onAddClick() {
         if (this.currLocation) {
+            this.locations = this.locations.filter(item => item !== this.currLocation);
             this.onLocationSelect({location: this.currLocation});
         }
     }
--- src/app/weather-item/weather-item.component.js
+++ src/app/weather-item/weather-item.component.js
@@ -2,8 +2,13 @@ import template from './weather-item.component.html';
 import './weather-item.component.css';
 
 class controller {
-    onCloseClick(id) {
-        this.onClose({id});
+    constructor($rootScope) {
+        this.$rootScope = $rootScope;
+    }
+
+    onCloseClick(weather) {
+        this.onClose();
+        this.$rootScope.$emit('onWeatherClose', weather);
     }
 }
 
