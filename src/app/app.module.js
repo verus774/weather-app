bad28a9c3cc6cf45dd949f09d64b3335ec8439de
Siarhei Ladutska <siarhei_ladutska@epam.com>
2019-01-14 15:03:57 +0300

    feat: add LocationSelectDirective

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -7,6 +7,7 @@ import WeatherListComponent from './weather-list/weather-list.component';
 import LocationSelectComponent from './location-select/location-select.component';
 import WeatherItemDirective from './weather-item-directive/weather-item.directive';
 import WeatherListDirective from './weather-list-directive/weather-list.directive';
+import LocationSelectDirective from './location-select-directive/location-select.directive';
 
 angular.module('weather-app', [])
     .factory('weatherService', weatherService)
@@ -15,4 +16,5 @@ angular.module('weather-app', [])
     .component('weatherList', WeatherListComponent)
     .component('locationSelect', LocationSelectComponent)
     .directive('weatherItemDir', WeatherItemDirective)
-    .directive('weatherListDir', WeatherListDirective);
+    .directive('weatherListDir', WeatherListDirective)
+    .directive('locationSelectDir', LocationSelectDirective);
---
+++ src/app/location-select-directive/location-select.directive.js
@@ -0,0 +1,30 @@
+import template from './location-select.directive.html';
+import './location-select.directive.css';
+
+export default ($rootScope, weatherService) => {
+    return {
+        restrict: 'E',
+        template,
+        scope: {
+            onLocationSelect: '&',
+        },
+        link: (scope, element, attrs) => {
+            scope.currLocation = '';
+            scope.locations = [];
+
+            weatherService.getLocations()
+                .then(locations => scope.locations = locations);
+
+            scope.onWeatherCloseUnsub = $rootScope.$on('onWeatherClose', (event, data) => {
+                scope.locations.push(data.location);
+            });
+
+            scope.onAddClick = () => {
+                if (scope.currLocation) {
+                    scope.locations = scope.locations.filter(item => item !== scope.currLocation);
+                    scope.onLocationSelect({location: scope.currLocation});
+                }
+            }
+        }
+    };
+}
